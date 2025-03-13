b6964698ccf2e36672619c1cb0a986d842e339fd
Francesc Tur <francescturcla@gmail.com>
2025-03-13 17:52:09 +0100

    afegits comentaris sobre la creació d'aquest repositori

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,10 @@
+# després de crear i configurar el git amb nom i email
+# git config --global user.name "nom"
+# git config --global user.email "email"
+# git init
+# git add .
+# es pot fer el commit directament amb la UI de VSCode
+# no se pq no tinc origin, no se pq serveix
+# git remote add origin
+
 print("hello world")
