b8235c783d9b7e16c255233741c631b697f52dd8
Francesc Tur <francescturcla@gmail.com>
2025-03-17 11:37:14 +0100

    keeping on with the same

--- artimetica.py
+++ artimetica.py
@@ -1,3 +1,7 @@
+import math
+# augmented assignment operators
+# +=, -=, *=, /=, **=, %=, //=
+
 animals = 0
 print(f"{animals} += 2 ->")
 animals += 2
@@ -16,4 +20,29 @@ animals **= 2
 print(animals)
 print(f"{animals} %= 2 ->")
 animals %= 2
-print(animals)
+print(f"{animals}\n")
+
+# // is the floor division operator which returns the integer part of the division
+
+# aritmetica 2
+
+x = 10
+y = 3.1425253
+z = -2
+
+print(f"x = {x}, y = {y}, z = {z}")
+
+print(f"round(y, 1) = {round(y, 1)}")
+print(f"round(y, 2) = {round(y, 2)}")
+print(f"round(y, 3) = {round(y, 3)}")
+print(f"abs(z) = {abs(z)}")
+print(f"pow(x, 2) = {pow(x, 2)}")
+print(f"x**2 = {x**2}")
+print(f"max(x, y, z) = {max(x, y, z)}\n")
+
+# math module
+print("pi = ", math.pi)
+print("e = ", math.e)
+print("math.sqrt(25) = ", math.sqrt(25))
+print("math.ceil(3.14) = ", math.ceil(3.14))
+print("math.floor(3.14) = ", math.floor(3.14))
---
+++ perimetreCircumferencia.py
@@ -0,0 +1,7 @@
+import math
+# 2 pi erre
+# 2 * pi * r
+
+r = float(input("Introduce the radius of the circle: "))
+perimetre = 2 * math.pi * r
+print(f"Perimetre of a circle with radius {r} is {perimetre}")
